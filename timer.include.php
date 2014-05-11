3380df46c262a3e93f4ea2845ef1a5edf2d4d93e
Robke22 <robrijken_92@hotmail.com>
2014-05-11 22:07:56 +0200

    update 11-05

--- timer.include.php
+++ timer.include.php
@@ -1,19 +1,3 @@
-<?php 
-include_once('class/restaurants.class.php');
-$restaurant = new Restaurant();
-
-if(empty($_SESSION['restaurant_id'])){
-	$result2 = $restaurant->getFirst();
-	
-	$_SESSION['restaurant_id'] = $result2['restaurant_id'];
-
-
-
-}
-
- ?>
-
-
 <script type="text/javascript">
 document.write ('<p id="date-time">', new Date().toLocaleString(), '<\/p>')
 if (document.getElementById) onload = function () {
